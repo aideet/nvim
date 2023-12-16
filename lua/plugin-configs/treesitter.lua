c51734518f4be5812efd90c09c5be8fe53f81037
Daniel Tscherwinka <tscherwinka@gmx.de>
2023-12-16 23:53:21 +0100

    treesitter added

---
+++ lua/plugin-configs/treesitter.lua
@@ -0,0 +1,23 @@
+local options = {
+    ensure_installed = { "lua", "rust", "go", "yaml", "markdown", "json", "http", "gitcommit", "gitignore", "vim", "vimdoc" },
+
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+    },
+
+    indent = { enable = true },
+
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+}
+
+-- folding see https://github.com/nvim-treesitter/nvim-treesitter#folding
+
+return options
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,7 +16,7 @@ return {
         opts = function()
             return require "plugin-configs.nvim-tree"
         end,
-        config = function()
+        config = function(_, opts)
             require("nvim-tree").setup {opts}
         end,
     },
@@ -27,8 +27,25 @@ return {
             "nvim-tree/nvim-web-devicons"
         },
         config = function()
-            return require "plugin-configs.lualine"
+            require "plugin-configs.lualine"
+        end,
+    },
+    { 
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = {},
+        config = function()
+            require("ibl").setup()
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        opts = function ()
+            return require "plugin-configs.treesitter"
+        end,
+        config = function (_, opts) 
+          require("nvim-treesitter.configs").setup(opts)
         end,
     }
-
 }
