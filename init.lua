7cd2bd11867daaae9a5af1db8f4680f6f2b18ce9
Daniel Tscherwinka <tscherwinka@gmx.de>
2023-12-20 21:43:50 +0100

    dadbod added

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- system clipboard support
+vim.opt.clipboard = 'unnamedplus'
 
 require('opts')
 require('functions')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -237,7 +237,28 @@ return {
     },
     {
         "iamcco/markdown-preview.nvim",
-        event = "BufEnter *.md",
+        cmd = {
+            'MarkdownPreview',
+            'MarkdownPreviewStop',
+            'MarkdownPreviewToggle',
+        },
         build = function() vim.fn["mkdp#util#install"]() end,
+    },
+    {
+        "kristijanhusak/vim-dadbod-ui",
+        dependencies = {
+            { 'tpope/vim-dadbod', lazy = true },
+            { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+        },
+        cmd = {
+            'DBUI',
+            'DBUIToggle',
+            'DBUIAddConnection',
+            'DBUIFindBuffer',
+        },
+        init = function()
+            -- Your DBUI configuration
+            vim.g.db_ui_use_nerd_fonts = 1
+        end,
     },
 }
