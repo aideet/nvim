9d1118e9617eb549523bce869333695f2a6579bc
Daniel Tscherwinka <tscherwinka@gmx.de>
2024-02-17 12:37:51 +0100

    dotenv linter tweaked

--- lua/plugin-configs/nvim-lint.lua
+++ lua/plugin-configs/nvim-lint.lua
@@ -1,7 +1,17 @@
 
 require('lint').linters_by_ft = {
-  yaml = {'yamllint',}
+  yaml = {'yamllint',},
+  sh = {'dotenv_linter',},
+  json = {'jsonlint',},
+  html = {'tidy',},
 } 
+
+local dotenv_linter = require('lint').linters.dotenv_linter
+dotenv_linter.args = {
+    '--quiet',
+    '--no-color',
+    "--skip=UnorderedKey",
+}
 
 vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
   callback = function()
