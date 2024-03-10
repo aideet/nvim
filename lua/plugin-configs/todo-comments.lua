bc64ec3370facb4fcf07b2990d3b4987b784e131
Daniel Tscherwinka <tscherwinka@gmx.de>
2024-03-10 21:36:45 +0100

    todo comment minor tweaks

--- lua/plugin-configs/todo-comments.lua
+++ lua/plugin-configs/todo-comments.lua
@@ -3,10 +3,10 @@ local options = {
     FIXME = {
       icon = " ", -- icon used for the sign, and in search results
       color = "error", -- can be a hex color, or a named color (see below)
-      alt = { "fixme", "BUG", "bug", "HACK", "hack" }, -- a set of other keywords that all map to this FIX keywords
+      alt = { "Fixme", "fixme", "BUG", "Bug", "bug", "HACK", "hack" }, -- a set of other keywords that all map to this FIX keywords
       -- signs = false, -- configure signs for some keywords individually
     },
-    TODO = { icon = " ", color = "info", alt = { "todo" } },
+    TODO = { icon = " ", color = "info", alt = { "Todo", "todo" } },
     -- HACK = { icon = " ", color = "warning", alt = { "hack" } },
     -- WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
     -- PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
