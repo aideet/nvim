dc0e9f8fba6556a9129497ad0bf2bdb81b3f039e
Daniel Tscherwinka <tscherwinka@gmx.de>
2024-03-12 09:54:32 +0100

    ufo tweaked

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -222,6 +222,7 @@ map('n', '<A-p>', '<Cmd>BufferPick<CR>', { silent = true, noremap = true })
 -- Todo Comment {{{
 map('n', '<leader>ot', ':TodoQuickFix<CR>', { silent = true, noremap = true})
 -- }}}
+-- ufo {{{
 set('n', 'zR', require('ufo').openAllFolds)
 set('n', 'zM', require('ufo').closeAllFolds)
 set('n', 'zr', require('ufo').openFoldsExceptKinds)
@@ -232,3 +233,4 @@ set('n', 'zz', function()
         vim.lsp.buf.hover()
     end
 end)
+-- }}}
