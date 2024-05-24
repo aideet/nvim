36d4ad53b8366592b1e244a9e5b636514b30d218
Daniel Tscherwinka <tscherwinka@gmx.de>
2024-05-25 00:10:04 +0200

    WhichKey mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,38 +3,164 @@
 local map = vim.api.nvim_set_keymap
 local set = vim.keymap.set
 
--- which-key {{{ 
 local wk = require("which-key")
--- wk.register({
---    ["<leader>"] = {
---        name = "Nvim Tree",
---        e = { ":NvimTreeFocus<CR>", "Focus Nvim Tree" },
---
---    }
--- })
+
+-- Normal Mode {{{
+wk.register({ 
+    ["<leader>w"] = { "<Cmd>WhichKey<CR>", "Which Key" },
+    ["<leader>H"] = { "<Cmd>noh<CR>", "Search Highlight Off" },
+    ["<leader>e"] = { ":NvimTreeFocus<CR>", "Nvim Tree Focus" },
+    ["<leader>E"] = { ":NvimTreeClose<CR>", "Nvim Tree Close" },
+    ["<leader>M"] = { "<CMD>RustLsp expandMacro<CR>", "Expand rust macro" },
+    ["<leader>^"] = { ":Startify<CR>", "Startify" },
+
+    -- Debug {{{
+    ["<f5>"] = { ":DapContinue<CR>", "Debug continue" },
+    ["<f6>"] = { ":DapTerminate<CR>", "Debug stop" },
+    ["<f7>"] = { ":DapStepInto<CR>", "Debug step into" },
+    ["<f8>"] = { ":DapStepOut<CR>", "Debug step out" },
+    ["<f9>"] = { ":DapStepOver<CR>", "Debug step over" },
+    ["<f10>"] = { ":DapPlayPause<CR>", "Debug play/pause" },
+    -- }}}
+
+    -- Window switch {{{
+    ["<C-h>"] = { "<C-w>h", "Window switch left" },
+    ["<C-l>"] = { "<C-w>l", "Window switch right" },
+    ["<C-j>"] = { "<C-w>j", "Window switch down" },
+    ["<C-k>"] = { "<C-w>k", "Window switch up" },
+    -- }}}
+
+    -- Goto / Git {{{
+    ["<leader>g"] = {
+       name = "Goto / Git",
+       x = { "<Cmd>silent execute '!xdg-open '.shellescape(expand('<cfile>'), 1)<CR>", "Open with system app" },
+       g = { "<Cmd>Git<CR>", "Git tool" },
+       a = { "<Cmd>Gitsigns toggle_current_line_blame<CR>", "Git annotations" },
+       c = { ":Telescope git_commits<CR>", "Git commits" },
+       s = { ":Telescope git_status<CR>", "Git status" },
+       b = { ":Telescope git_branches<CR>", "Git branches" },
+    },
+    -- }}}
+
+    -- Line numbers {{{
+    ["<leader>n"] = {
+       name = "Line Numbers",
+       n = { ":set nu!<CR>", "Toggle line numbers" },
+       r = { ":set rnu!<CR>", "Toggle relative line numbers" },
+    },
+    -- }}}
+
+    -- Markdown preview {{{
+    ["<leader>m"] = {
+       name = "Markdown Preview",
+       p = { ":MarkdownPreview<CR>", "Preview" },
+       s = { ":MarkdownPreviewStop<CR>", "Preview stop" },
+       t = { ":MarkdownPreviewToggle<CR>", "Preview toggle" },
+    },
+    -- }}}
+
+    -- Find {{{
+    ["<leader>f"] = {
+       name = "Find",
+       p = { ":NnnPicker<CR>", "File picker" },
+       e = { ":NnnExplorer<CR>", "File explorer" },
+       f = { ":Telescope find_files<CR>", "Find files" },
+       a = { ":Telescope find_files follow=true no_ignore=true hidden=true<CR>", "Find hidden files" },
+       w = { ":Telescope live_grep<CR>", "Grep" },
+       b = { ":Telescope buffers<CR>", "Find buffer" },
+       h = { ":Telescope help_tags<CR>", "Find help tags" },
+       o = { ":Telescope oldfiles<CR>", "Find old files" },
+       z = { ":Telescope current_buffer_fuzzy_find<CR>", "Find fuzzy in buffer" },
+       l = { ":Telescope spell_suggest<CR>", "Find spelling" },
+       m = { ":Telescope marks<CR>", "Find marks" },
+       r = { ":Telescope registers<CR>", "Find registers" },
+       s = { ":Telescope search_history<CR>", "Find search history" },
+       k = { ":Telescope keymaps<CR>", "Find keymaps" },
+       t = { ":TodoTelescope<CR>", "Find todos" },
+    },
+    -- }}}
+
+    -- Lsp Telescope {{{
+    ["<leader>t"] = {
+       name = "LSP Telescope",
+       p = { ":Telescope diagnostics<CR>", "Diagnostics" },
+       d = { ":Telescope lsp_definitions<CR>", "Definitions" },
+       s = { ":Telescope lsp_document_symbols<CR>", "Document symbols" },
+       S = { ":Telescope lsp_document_symbols ignore_symbols=variable<CR>", "Document symbols (no vars)" },
+       w = { ":Telescope lsp_workspace_symbols<CR>", "Workspace symbols" },
+       W = { ":Telescope lsp_dynamic_workspace_symbols<CR>", "Dynamic document symbols" },
+       m = { ":Telescope lsp_implementations<CR>", "Implementations" },
+       i = { ":Telescope lsp_incoming_calls<CR>", "Incoming calls" },
+       o = { ":Telescope lsp_outgoing_calls<CR>", "Outgoing calls" },
+       r = { ":Telescope lsp_references<CR>", "References" },
+       x = { ":Telescope lsp_type_definitions<CR>", "Type definitions" },
+    },
+    -- }}}
+
+    -- Crates {{{
+    ["<leader>c"] = {
+       name = "Crates",
+       t = { "<CMD>lua require('crates').toggle()<CR>", "Toggle annotations" },
+       r = { "<CMD>lua require('crates').reload()<CR>", "Reload" },
+       v = { "<CMD>lua require('crates').show_versions_popup()<CR>", "Show version popup" },
+       f = { "<CMD>lua require('crates').show_features_popup()<CR>", "Show features popup" },
+       d = { "<CMD>lua require('crates').show_dependencies_popup()<CR>", "Show dependencies popup" },
+       u = { "<CMD>lua require('crates').update_crate()<CR>", "Update crate" },
+       a = { "<CMD>lua require('crates').update_all_crates()<CR>", "Update all crates" },
+       U = { "<CMD>lua require('crates').upgrade_crate()<CR>", "Upgrade crate" },
+       A = { "<CMD>lua require('crates').upgrade_all_crates()<CR>", "Upgrade all crates" },
+       x = { "<CMD>lua require('crates').expand_plain_crate_to_inline_table()<CR>", "Expand to inline table" },
+       X = { "<CMD>lua require('crates').extract_crate_into_table()<CR>", "Extract into table" },
+       H = { "<CMD>lua require('crates').open_homepage()<CR>", "Homepage" },
+       R = { "<CMD>lua require('crates').open_repository()<CR>", "Repository" },
+       D = { "<CMD>lua require('crates').open_documentation()<CR>", "Documentation" },
+       C = { "<CMD>lua require('crates').open_crates_io()<CR>", "Crates.io" },
+    },
+    -- }}}
+
+    -- Debug {{{
+    ["<leader>d"] = {
+       name = "Debug",
+       b = { ":DapToggleBreakpoint<CR>", "Toggle breakpoint" },
+       r = { ":DapToggleRepl<CR>", "Toggle Repl" },
+       f = { ":DapUiFloat<CR>", "UI float" },
+       u = { ":DapUiToggle<CR>", "UI toggle" },
+    },
+    -- }}}
+
+    -- DB {{{
+    ["<leader>p"] = {
+       name = "DB",
+       u = { ":DBUIToggle<CR>", "DB UI toggle" },
+       f = { ":DBUIFindBuffer<CR>", "DB find buffer" },
+    },
+    -- }}}
+
+})
 -- }}}
+
+
+-- Visual Mode {{{
+wk.register({ -- Visual Mode
+    -- Crates {{{
+    ["<leader>c"] = {
+       name = "Crates",
+       u = { "<CMD>lua require('crates').update_crate()<CR>", "Update crate" },
+       U = { "<CMD>lua require('crates').upgrade_crate()<CR>", "Upgrade crate" },
+    },
+    -- }}}
+
+    -- DB {{{
+    ["<leader>p"] = {
+       name = "DB",
+       e = { ":DB<CR>", "DB execute" },
+    },
+    -- }}}
+}, { mode = "v"})
+-- }}}
+
 -- escape to NORMAL mode {{{ 
 set('i', 'jk', '<Esc>', { noremap = true })
--- }}}
--- Open with system app {{{
-map('n', 'gx', "<Cmd>silent execute '!xdg-open '.shellescape(expand('<cfile>'), 1)<CR>", { silent = true, noremap = true })
--- }}}
--- un-highlight search occurrences {{{
-map('n', '<leader>H', '<Cmd>noh<CR>', { silent = true, noremap = true })
--- }}}
--- Nvim Tree {{{
-map('n', '<leader>e', ':NvimTreeFocus<CR>', { silent = true, noremap = true })
-map('n', '<leader>E', ':NvimTreeClose<CR>', { silent = true, noremap = true })
--- }}}
--- nnn file browser {{{
-map('n', '<leader>np', ':NnnPicker<CR>', { silent = true, noremap = true })
-map('n', '<leader>ne', ':NnnExplorer<CR>', { silent = true, noremap = true })
--- }}}
--- window navigation {{{
-map('n', '<C-h>', '<C-w>h', { noremap = true })
-map('n', '<C-l>', '<C-w>l', { noremap = true })
-map('n', '<C-j>', '<C-w>j', { noremap = true })
-map('n', '<C-k>', '<C-w>k', { noremap = true })
 -- }}}
 -- buffer navigation {{{
 map('i', '<C-h>', '<Left>', { noremap = true })
@@ -42,28 +168,13 @@ map('i', '<C-l>', '<Right>', { noremap = true })
 map('i', '<C-j>', '<Down>', { noremap = true })
 map('i', '<C-k>', '<Up>', { noremap = true })
 
+-- scrolling
 set({'n', 'i'}, '<A-k>', '<C-y>', { noremap = true })
 set({'n', 'i'}, '<A-j>', '<C-e>', { noremap = true })
--- }}}
--- line numbers {{{
-map('n', '<leader>n', ':set nu!<CR>', { silent = true, noremap = true })
-map('n', '<leader>rn', ':set rnu!<CR>', { silent = true, noremap = true })
 -- }}}
 -- Float term {{{
 map('n', '<A-i>', ':lua require("FTerm").toggle()<CR>', { silent = true, noremap = true })
 map('t', '<A-i>', '<C-\\><C-n>:lua require("FTerm").toggle()<CR>', { silent = true, noremap = true }) -- see https://www.reddit.com/r/neovim/comments/cger8p/how_quickly_close_a_terminal_buffer/
--- }}}
--- startify {{{
-map('n', '<leader>^', ':Startify<CR>', { silent = true, noremap = true }) 
--- }}}
--- fugitive {{{
-map('n', '<leader>gg', '<Cmd>Git<CR>', { silent = true, noremap = true })
--- }}}
--- GitSigns {{{
-map('n', '<leader>ga', '<Cmd>Gitsigns toggle_current_line_blame<CR>', { silent = true, noremap = true })
--- }}}
--- LSP float documentation {{{
--- set('n', 'K', show_documentation, { silent = true }) -- obsolete with vim.lsp.buf.hover
 -- }}}
 -- LSP {{{
 -- Global mappings.
@@ -82,7 +193,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- Enable completion triggered by <c-x><c-o>
         vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-        local lsp_opts = {buffer = ev.buf }
+        -- local lsp_opts = {buffer = ev.buf }
+        -- local wk = require("which-key")
+        -- wk.register({
+        --    ["g"] = {
+        --        name = "LSP Goto",
+        --        D = { "vim.lsp.buf.declaration", "Goto declaration" },
+        --        d = { "vim.lsp.buf.definition", "Goto definition" },
+        --        i = { "vim.lsp.buf.implementation", "Goto implementation" },
+        --        r = { "vim.lsp.buf.references", "Goto references" },
+        --        t = { "vim.lsp.buf.type_definition", "Goto type definition" },
+        --    }
+        -- }, lsp_opts )
         set('n', 'gD', vim.lsp.buf.declaration, lsp_opts)
         set('n', 'gd', vim.lsp.buf.definition, lsp_opts)
         set('n', 'gi', vim.lsp.buf.implementation, lsp_opts)
@@ -105,37 +227,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
         end, lsp_opts)
     end,
 })
--- }}}
--- Rust {{{
-set('n', '<leader>me', '<CMD>RustLsp expandMacro<CR>')
--- }}}
--- crates {{{
-local crates = require("crates")
-local crates_opts = { silent = true }
-
-set("n", "<leader>ct", crates.toggle, crates_opts)
-
-set("n", "<leader>cr", crates.reload, crates_opts)
-
-set("n", "<leader>cv", crates.show_versions_popup, crates_opts)
-set("n", "<leader>cf", crates.show_features_popup, crates_opts)
-set("n", "<leader>cd", crates.show_dependencies_popup, crates_opts)
-
-set("n", "<leader>cu", crates.update_crate, crates_opts)
-set("v", "<leader>cu", crates.update_crates, crates_opts)
-set("n", "<leader>ca", crates.update_all_crates, crates_opts)
-set("n", "<leader>cU", crates.upgrade_crate, crates_opts)
-set("v", "<leader>cU", crates.upgrade_crates, crates_opts)
-set("n", "<leader>cA", crates.upgrade_all_crates, crates_opts)
-
-set("n", "<leader>cx", crates.expand_plain_crate_to_inline_table, crates_opts)
-set("n", "<leader>cX", crates.extract_crate_into_table, crates_opts)
-
-set("n", "<leader>cH", crates.open_homepage, crates_opts)
-set("n", "<leader>cR", crates.open_repository, crates_opts)
-set("n", "<leader>cD", crates.open_documentation, crates_opts)
-set("n", "<leader>cC", crates.open_crates_io, crates_opts)
--- }}}
 -- Comment {{{
 set('n', '<leader>/', function()
         require("Comment.api").toggle.linewise.current()
@@ -150,54 +241,7 @@ set('n', '<C-7>', function()
       {}
 )
 -- }}}
--- Telescope {{{
-map('n', '<leader>ff', ':Telescope find_files<CR>', { silent = true, noremap = true})
-map('n', '<leader>fa', ':Telescope find_files follow=true no_ignore=true hidden=true<CR>', { silent = true, noremap = true})
-map('n', '<leader>fw', ':Telescope live_grep<CR>', { silent = true, noremap = true})
-map('n', '<leader>fb', ':Telescope buffers<CR>', { silent = true, noremap = true})
-map('n', '<leader>fh', ':Telescope help_tags<CR>', { silent = true, noremap = true})
-map('n', '<leader>fo', ':Telescope oldfiles<CR>', { silent = true, noremap = true})
-map('n', '<leader>fz', ':Telescope current_buffer_fuzzy_find<CR>', { silent = true, noremap = true})
-map('n', '<leader>fl', ':Telescope spell_suggest<CR>', { silent = true, noremap = true})
--- map('n', '<leader>fp', ':Telescope terms<CR>', { silent = true, noremap = true})
-map('n', '<leader>fm', ':Telescope marks<CR>', { silent = true, noremap = true})
-map('n', '<leader>fr', ':Telescope registers<CR>', { silent = true, noremap = true})
-map('n', '<leader>fs', ':Telescope search_history<CR>', { silent = true, noremap = true})
-map('n', '<leader>fk', ':Telescope keymaps<CR>', { silent = true, noremap = true})
-map('n', '<leader>ft', ':TodoTelescope<CR>', { silent = true, noremap = true})
-
-map('n', '<leader>gc', ':Telescope git_commits<CR>', { silent = true, noremap = true})
-map('n', '<leader>gs', ':Telescope git_status<CR>', { silent = true, noremap = true})
-map('n', '<leader>gb', ':Telescope git_branches<CR>', { silent = true, noremap = true})
-
-map('n', '<leader>tp', ':Telescope diagnostics<CR>', { silent = true, noremap = true})
-map('n', '<leader>td', ':Telescope lsp_definitions<CR>', { silent = true, noremap = true})
-map('n', '<leader>ts', ':Telescope lsp_document_symbols<CR>', { silent = true, noremap = true})
-map('n', '<leader>tS', ':Telescope lsp_document_symbols ignore_symbols=variable<CR>', { silent = true, noremap = true})
-map('n', '<leader>tw', ':Telescope lsp_workspace_symbols<CR>', { silent = true, noremap = true})
-map('n', '<leader>tW', ':Telescope lsp_dynamic_workspace_symbols<CR>', { silent = true, noremap = true})
-map('n', '<leader>tm', ':Telescope lsp_implementations<CR>', { silent = true, noremap = true})
-map('n', '<leader>ti', ':Telescope lsp_incoming_calls<CR>', { silent = true, noremap = true})
-map('n', '<leader>to', ':Telescope lsp_outgoing_calls<CR>', { silent = true, noremap = true})
-map('n', '<leader>tr', ':Telescope lsp_references<CR>', { silent = true, noremap = true})
-map('n', '<leader>tt', ':Telescope lsp_type_definitions<CR>', { silent = true, noremap = true})
--- }}}
--- Markdown preview {{{
-map('n', '<leader>mdp', ':MarkdownPreview<CR>', { silent = true, noremap = true})
-map('n', '<leader>mds', ':MarkdownPreviewStop<CR>', { silent = true, noremap = true})
-map('n', '<leader>mdt', ':MarkdownPreviewToggle<CR>', { silent = true, noremap = true})
--- }}}
 -- dap {{{
-map('n', '<leader>db', ':DapToggleBreakpoint<CR>', { silent = true, noremap = true})
-map('n', '<f5>', ':DapContinue<CR>', { silent = true, noremap = true})
-map('n', '<f6>', ':DapTerminate<CR>', { silent = true, noremap = true})
-map('n', '<f7>', ':DapStepInto<CR>', { silent = true, noremap = true})
-map('n', '<f8>', ':DapStepOut<CR>', { silent = true, noremap = true})
-map('n', '<f9>', ':DapStepOver<CR>', { silent = true, noremap = true})
-map('n', '<f10>', ':DapPlayPause<CR>', { silent = true, noremap = true})
-map('n', '<leader>dr', ':DapToggleRepl<CR>', { silent = true, noremap = true})
-map('n', '<leader>df', ':DapUiFloat<CR>', { silent = true, noremap = true})
-map('n', '<leader>du', ':DapUiToggle<CR>', { silent = true, noremap = true})
 set('n', '<leader>ds', function ()
             local widgets = require('dap.ui.widgets');
             local sidebar = widgets.sidebar(widgets.scopes);
@@ -205,13 +249,6 @@ set('n', '<leader>ds', function ()
         end,
         { silent = true, noremap = true}
     )
--- }}}
--- dap-rust {{{
--- see lsp-rust.lua
--- }}}
--- Dadbod DB Client {{{
-map('n', '<leader>pu', ':DBUIToggle<CR>', { silent = true, noremap = true})
-map('v', '<leader>pe', ':DB<CR>', { silent = true, noremap = true})
 -- }}}
 -- barbar (tabs) {{{
 map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', { silent = true, noremap = true })
