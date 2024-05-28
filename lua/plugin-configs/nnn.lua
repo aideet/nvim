adb99f2049f1a15bca38f2b79cf9470edff11ac0
Daniel Tscherwinka <tscherwinka@gmx.de>
2024-05-28 15:37:49 +0200

    nnn theme and plugins configured

--- lua/plugin-configs/nnn.lua
+++ lua/plugin-configs/nnn.lua
@@ -1,8 +1,8 @@
 
 
-vim.env.NNN_PLUG='f:finder;o:fzopen;p:preview-tui;d:diffs;t:nmount;v:imgview'
+vim.env.NNN_PLUG='o:fzopen;p:preview-tui;d:diffs'
 vim.env.NNN_FIFO='/tmp/nnn.fifoi'
-vim.env.NNN_FCOLORS='00006f310000000000000000'
+vim.env.NNN_FCOLORS='00000C48009393F7C4000000'
 vim.env.NNN_TERMINAL='kitty'
 vim.env.NNN_BMS="d:/home/dts/doc;w:/home/dts/dev/ws_dts;m:/home/dts/media/music;a:/home/dts/media/audibles:p:/home/dts/media/pics;v:/home/dts/media/video"
 
